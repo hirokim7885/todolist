2ace283359dda420e9a6400ca9b1c2cc25286284
hirokim7885 <hirokim7885@gmail.com>
2021-07-17 23:01:57 +0900

    新規及び編集タスクの実装

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,52 +2,51 @@ class ListsController < ApplicationController
     before_action :set_task, only: %i[ show edit update destroy ]
 
     def index
-        @tasks = Task.all.order(created_at: 'desc')
+        @tasks = current_account.tasks.all.order(created_at: 'desc')
     end
 
     def show
-        @task = Task.find(params[:id])
+        @task = current_account.tasks.find(params[:id])
     end
 
     def new
-        @task = Task.new
+        @task = current_account.tasks.new
     end
 
     def create
-        @task = Task.new(task_params)
+        @task = current_account.tasks.new(task_params)
         if @task.save
-        redirect_to @task, notice: '新規タスクを保存しました。'
+            redirect_to list_path(@task), notice: '新規タスクを保存しました。'
         else
-        render 'new', status: :unprocessable_entity
+            render 'new', status: :unprocessable_entity
         end
     end
 
     def edit
-        @task = Task.find(params[:id])
+        @task = current_account.tasks.find(params[:id])
     end
 
     def update
-        @task = Task.find(params[:id])
+        @task = current_account.tasks.find(params[:id])
         if @task.update(task_params)
-            redirect_to @task, notice: 'タスクを更新しました。'
+            redirect_to list_path(@task), notice: 'タスクを更新しました。'
         else
             render 'edit', status: :unprocessable_entity
         end
     end
 
     def destroy
-        @task = Task.find(params[:id])
+        @task = current_account.tasks.find(params[:id])
         @task.destroy
         redirect_to lists_path, notice; 'タスクを削除しました。'
     end
 
     private
         def set_task
-            @task = Task.find(params[:id])
+            @task = current_account.tasks.find(params[:id])
         end
 
         def task_params
-            params.require(:task).permit(:title, :body, :deadline, :priority, :category )
+            params.require(:task).permit( :title, :body, :deadline, :priority_id, :category )
         end
-
 end
